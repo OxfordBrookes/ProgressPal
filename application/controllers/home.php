d8e4f7e666e657a96d7670a3d583acb4f1b39f21
Aaron Stillwell <stillwell.mailbox@gmail.com>
2013-04-27 16:51:59 +0100

    Added sign in controller + test view

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,27 +1,33 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Class Home
+ *
+ * Sign in page.
+ */
 class Home extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+    /**
+     * Constructor.
+     *
+     * Check if the user is already logged in and direct them elsewhere
+     * if necessary.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($this->session->userdata('user_id'))
+        {
+            // User is already logged in, send them somewhere else!
+        }
+    }
+
+    /**
+     * Show the sign in page.
+     */
+    public function index()
 	{
-		echo "Hello!";
+		$this->load->view('signin');
 	}
 }
-
-/* End of file home.php */
-/* Location: ./application/controllers/home.php */

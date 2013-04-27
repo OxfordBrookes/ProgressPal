2d2d91e58ede7e1f22667d2f906b5fb9df28879b
Jagdeep <jagdeepnagpal@gmail.com>
2013-04-27 23:32:18 +0100

    added dashboard functions
    started

--- application/controllers/student/dashboard.php
+++ application/controllers/student/dashboard.php
@@ -12,7 +12,7 @@ class Dashboard extends CI_Controller {
      */
     public function __construct()
     {
-        $this->load->model(array('module_m', 'task_m', 'milestone_m'));
+        $this->load->model(array('module_m', 'task_m', 'milestone_m', 'user_m', 'enrollment_m', 'assignment_m'));
     }
 
     /**
@@ -22,5 +22,44 @@ class Dashboard extends CI_Controller {
     {
 
     }
+    
+    //getAvgModuleProgress($moduleID) -> int avgModule
+    public function getClassAvgModuleProgress($moduleID)
+    {
+        $modules = $this->module_m->get_many_by('module_id',$moduleID);
+        
+        
+        $assignmentAvg = getAvgAssignemntProgress($assignmentID);
+        $moduleAvg;
+        return $moduleAvg;
+    }
+    
+    //getAvgAssignmentProgress($assignmentID)
+    public function getClassAvgAssignmentProgress($assignmentID)
+    {
+        $assignmentAvg;
+        return $assignmentAvg;
+    }
+    
+    
+    public function isAssignementCompleted($assignmentID)
+    {
+        return false;
+    }
+    
+    public function isMilestoneCompleted($milestoneID)
+    {
+        return false;
+    }
+    
 
+    //getUserModuleProgress()
+    //getUserAssignmentProgress()
+    //getUserClassProgress()
+ 
+    //getUserProgress($userid) -> int userProgress
+    
+    //getData($userID) -> JSON {modules, assignments, milestones}
+    
+    //getProgress($userid) -> int getUserProgress & int 
 }

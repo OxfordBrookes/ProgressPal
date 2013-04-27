0748aa6a5f2078533511b67828aa40e16efdfc4b
Aaron Stillwell <stillwell.mailbox@gmail.com>
2013-04-27 21:35:41 +0100

    Added password hash

--- application/models/user_m.php
+++ application/models/user_m.php
@@ -4,4 +4,17 @@ class User_m extends MY_Model {
 
     protected $_table = 'user';
 
+    /**
+     * Produce a password hash.
+     *
+     * @param $password
+     * @param $email
+     * @param $created
+     */
+    public function hash($password, $email, $created)
+    {
+        $password = str_split($password, (strlen($password) / 2) + 1);
+        return hash('whirlpool', $email.$password[0] . 'bananarama' . $password[1] . 'dska2' . $created);
+    }
+
 }

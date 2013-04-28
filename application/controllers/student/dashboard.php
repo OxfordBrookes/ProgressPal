2aaf530f139b82ab6455d3285a1f428715432cbf
Jagdeep <jagdeepnagpal@gmail.com>
2013-04-28 04:15:57 +0100

    updated getProgress

--- application/controllers/student/dashboard.php
+++ application/controllers/student/dashboard.php
@@ -7,6 +7,7 @@
  */
 class Dashboard extends Student_Controller {
 
+    public $totalMilestones = 0;
     /**
      * Constructor.
      */
@@ -67,8 +68,13 @@ class Dashboard extends Student_Controller {
         return ($numComp/count($milestones))*100;
     }
     
+    public function isModuleCompleted($moduleID)
+    {
+        return false;
+    }
     
-    public function isAssignementCompleted($assignmentID)
+    
+    public function isAssignmentCompleted($assignmentID)
     {
         $milestones = $this->milestone_m->get_many_by('parent_assignment_id',$assignmentID);
         $completed = TRUE;
@@ -94,19 +100,20 @@ class Dashboard extends Student_Controller {
 
     public function getUserModuleProgress($userID, $moduleID){
         $user = $this->user_m->get_back('user_id',$userID);
-        
     }
     //getUserAssignmentProgress()
     //getUserClassProgress()
  
     //getUserProgress($userid) -> int userProgress
-    public function getUserProgress($userID)
+    public function getProgress($userID)
     {
-        
+        //echo json_encode($userID,$this->getClassAvgModuleProgress,$this->$totalMilestones);
+        echo json_encode($userID,12,20);       
     }
     
     public function getData($userID)
     {
+        
         $data = array(array(
            'id' => 1,
            'type' => 'module',

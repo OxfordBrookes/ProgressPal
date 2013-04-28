f097d5d8648035f269cdc415de0bd46a2b07df1d
Tim <12038068@brookes.ac.uk>
2013-04-28 02:31:41 +0100

    Update
    More functions done

--- application/controllers/student/dashboard.php
+++ application/controllers/student/dashboard.php
@@ -44,34 +44,55 @@ class Dashboard extends CI_Controller {
     //getAvgModuleProgress($moduleID) -> int avgModule
     public function getClassAvgModuleProgress($moduleID)
     {
-        $assignments = $this->assignment_m->get_many_by('',$moduleID);
-        $assignmentAvg = getAvgAssignemntProgress($assignmentID);
-        $moduleAvg;
-        return $moduleAvg;
+        $assignments = $this->assignment_m->get_many_by('parent_module_id',$moduleID);
+        $moduleAvg = 0;
+        for($i=0; i<count($assignments);$i++){
+            $moduleAvg += getAvgAssignemntProgress($assignments[i]['assignment_id']);
+        }
+        return ($moduleAvg/(count($assignments)*100))*100;
     }
     
     //getAvgAssignmentProgress($assignmentID)
     public function getClassAvgAssignmentProgress($assignmentID)
     {
         $milestones = $this->milestone_m->get_many_by('parent_assignment_id',$assignmentID);
-        var_dump($milestones);
-        //for($i=0; $assignmentID ){}
-        return $assignmentAvg;
+        $numComp = 0;
+        for($i=0;i<count($milestones);$i++){
+            if($milestones[i]['is_completed']){
+                $numComp++;
+            }
+        }
+        return ($numComp/count($milestones))*100;
     }
     
     
     public function isAssignementCompleted($assignmentID)
     {
-        return false;
+        $milestones = $this->milestone_m->get_many_by('parent_assignment_id',$assignmentID);
+        $completed = TRUE;
+        for($i=0;i<count($milestones);$i++){
+            if(!$milestones[i]['is_completed']){
+                $completed=FALSE;
+            }
+        }
+        return $completed;
     }
     
     public function isMilestoneCompleted($milestoneID)
     {
-        return false;
+        $milestone = $this->milestone_m->get_back('milestone_id',$milestoneID);
+        $completed = FALSE;
+        if($milestone['is_completed']){
+            $completed = TRUE;
+            }
+        return $completed;
     }
     
 
-    //getUserModuleProgress()
+    public function getUserModuleProgress($userID, $moduleID){
+        $user = $this->user_m->get_back('user_id',$userID);
+        
+    }
     //getUserAssignmentProgress()
     //getUserClassProgress()
  
@@ -105,3 +126,5 @@ class Dashboard extends CI_Controller {
     
     //getProgress($userid) -> int getUserProgress & int 
 }
+
+

3794959ac0314873ca0bd95cb06ac0a4a04cb155
Jagdeep <jagdeepnagpal@gmail.com>
2013-04-28 06:59:59 +0100

    updated config

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "progresspal_";
-$config['cookie_domain']	= "127.0.0.1";
+$config['cookie_domain']	= "progresspal.azurewebsites.net";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 

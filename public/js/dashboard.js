61fc0bd9f81a850f9f1d14398ecd881b6f408cd1
Jagdeep Nagpal <jagdeepnagpal@gmail.com>
2013-04-28 08:13:55 +0100

    lkjn

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,8 +4,6 @@ var USER_ID = 1;
 (function (window, document, $, baseUrl, userId, undefined) {
     "use strict";
 
-    baseUrl = (baseUrl[baseUrl.length - 1] === "/") ? baseUrl : baseUrl + "/";
-    console.log(baseUrl);
     // Load progress bar data.
     $.getJSON(baseUrl + "getProgress/" + userId, function (progress) {
         var userProgress = (progress.user / progress.total) * 100;

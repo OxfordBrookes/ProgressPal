d3ecc8aa7ba5f5ef27c8ad454b615e6e7c125acc
Ryan Smith <0ryansmith1994@gmail.com>
2013-04-29 16:00:29 +0200

    Referenced minified dashboard JavaScript file.

--- application/views/student/dashboard.php
+++ application/views/student/dashboard.php
@@ -30,7 +30,7 @@
         </script>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src="<?php echo base_url('js/bootstrap.min.js') ?>"></script>
-		<script src="<?php echo base_url('js/dashboard.js') ?>"></script>
+		<script src="<?php echo base_url('js/dashboard.min.js') ?>"></script>
             
 	</body>
 </html>
